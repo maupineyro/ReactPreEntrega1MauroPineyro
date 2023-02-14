3b3a0654410240cbcd7d04815b7c5a815fedcab3
maupineyro <argmau357@gmail.com>
2023-02-14 01:40:29 -0300

    greeting

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <ItemListContainer />
+      <ItemListContainer greeting="Bienvenido a REACTIVE, este es el ItemListContainer" />
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const ItemListContainer = () => {
+const ItemListContainer = ({ greeting }) => {
     return (
-        <div className="mx-5 my-5">este es el ItemListContainer, no hay productos actualmente</div>
+        <div className="mx-5 my-5">{greeting}</div>
 
     )
 }
